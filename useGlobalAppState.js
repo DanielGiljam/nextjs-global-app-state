a71be233eae763aacb3a0df0df3959d871604fc8
Daniel Giljam <33825175+DanielGiljam@users.noreply.github.com>
2020-02-20 12:22:24 +0200

    Adds CHANGELOG.md
    - Adds "private" field to package.json.
    - Removes useGlobalAppState.js and useStrings.js

--- useGlobalAppState.js
+++
@@ -1,4 +0,0 @@
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-const useGlobalAppState = require("./lib/contexts/global-app-state/useGlobalAppState")
-
-module.exports = useGlobalAppState
