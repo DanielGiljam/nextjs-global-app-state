2924df9d950cf6e90b90dbb015eb64d8980298f7
Daniel Giljam <33825175+DanielGiljam@users.noreply.github.com>
2020-02-24 16:18:36 +0200

    Removes src/contexts/global-app-state

--- src/contexts/global-app-state/useGlobalAppState.ts
+++
@@ -1,7 +0,0 @@
-import {useContext} from "react"
-
-import {GlobalAppStateContext, GlobalAppState} from "./index"
-
-export default function useGlobalAppState(): GlobalAppState {
-  return useContext(GlobalAppStateContext)
-}
